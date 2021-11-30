68536ba0cbbbd8227327f86d6e85dc36f15408ba
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-11-30 17:16:24 +0100

    try to fix backend working with heroku

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const {
 } = require('mongodb');
 require('dotenv').config();
 const bodyParser = require('body-parser');
+const {
+    response
+} = require('express');
 const app = express();
 const port = process.env.PORT;
 const client = new MongoClient(process.env.FINAL_URL);
 
+<<<<<<< Updated upstream
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
@@ -24,3 +28,7 @@ app.get('/', (req, res) => {
 app.get('/public', (req, res) => {
     res.send(`./index.html`);
 })
+=======
+app.use(bodyParser.json());
+app.use(express.static("public"));
+>>>>>>> Stashed changes
