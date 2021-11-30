38be85bf229e92bc9d6a3c610b026ad961e77405
MatthiasVandeCasteele <71661057+Matthias-VdC@users.noreply.github.com>
2021-11-30 15:42:55 +0100

    Added body parser

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const {
     MongoClient
 } = require('mongodb');
 require('dotenv').config();
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT;
-
-
 const client = new MongoClient(process.env.FINAL_URL);
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(bodyParser.json());
+app.use(express.static("public"));
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at ${port}`)
+});
+
+app.get('/', (req, res) => {
+    console.log(req);
+    res.send(`Data received with id: ${req.body.id}`);
+});
+
+app.get('/public', (req, res) => {
+    res.send(`./index.html`);
 })
