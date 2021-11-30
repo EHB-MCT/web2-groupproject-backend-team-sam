f4f8d71fabd60c3007b64c0d232bba0dc490aa41
SamBuseyne <sam.buseyne@student.ehb.be>
2021-11-30 16:29:51 +0100

    Session_7_database
    Restructured files

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const {
     MongoClient
 } = require("mongodb");
-const url = "mongodb+srv://admin:admin@cluster0.yc2s0.mongodb.net/session7?retryWrites=true&w=majority";
-const client = new MongoClient(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-client.connect(err => {
-    const collection = client.db("session7").collection("Levels");
-    // perform actions on the collection object
-    console.log("Hosting")
-    client.close();
-});
+
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = "mongodb+srv://admin:admin@cluster0.yc2s0.mongodb.net/session7?retryWrites=true&w=majority"
+const client = new MongoClient(url);
+
+// The database to use
+const dbName = "Session7";
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+
+        // Use the collection "Levels"
+        const col = db.collection("Challenges");
+
+        // Construct a document                                                                                                                                                              
+        let detailsDocument = {
+            name: "Make an HTML-element",
+            points: 5,
+            course: "Kickstart",
+            session: 1,
+        }
+
+        // Insert a single document, wait for promise so we can read it back
+        const p = await col.insertOne(detailsDocument);
+        // Find one document
+        const myDoc = await col.findOne();
+        // Print to the console
+        console.log(myDoc);
+
+    } catch (err) {
+        console.log(err.stack);
+    } finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
